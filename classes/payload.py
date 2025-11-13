4abab0a776cd3657e3b0b276169240a348fe8563
Andrea Grimandi <andrea.grimandi1@protonmail.ch>
2025-11-13 11:40:39 +0100

    Refactor JiraRequestPayload to improve JSON serialization

--- classes/payload.py
+++ classes/payload.py
@@ -2,17 +2,14 @@ import json
 
 
 class JiraRequestPayload:
-    timeSpentSeconds: int
-    comment: str
-    started: str
-
-    def __new__(cls, *args, **kwargs):
-        return super().__new__(cls)
-
     def __init__(self, timeSpentSeconds, comment, started):
         self.timeSpentSeconds = timeSpentSeconds
         self.comment = comment
         self.started = started
 
     def toJsonRequest(self):
-        return json.dump(self.__dict__)
+        return {
+            "timeSpentSeconds": self.timeSpentSeconds,
+            "comment": self.comment,
+            "started": self.started
+        }
